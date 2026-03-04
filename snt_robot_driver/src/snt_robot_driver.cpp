b80898899e0ab5bbfbbd3748ce352931a89f4f4c
Sepose <well_77@naver.com>
2026-03-04 14:00:46 +0900

    fix

--- snt_robot_driver/src/snt_robot_driver.cpp
+++ snt_robot_driver/src/snt_robot_driver.cpp
@@ -28,8 +28,8 @@ public:
   // ---------------- on_init ----------------
   CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override
   {
-    RCLCPP_ERROR(rclcpp::get_logger("SNT_HW"), "SNT로봇: write() 테스트 에러 로그입니다");
     RCLCPP_INFO(rclcpp::get_logger("SNT_HW"), "SNT로봇 on_init 작동 - 초기화 시작.");
+
     if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
     {
       RCLCPP_ERROR(rclcpp::get_logger("SNT_HW"), "SNT로봇 on_init 실패 - HardwareInfo 로드 실패.");
@@ -37,6 +37,8 @@ public:
     }
 
     num_joints_ = info_.joints.size();
+
+    //
     position_.assign(num_joints_, 0.0);
     velocity_.assign(num_joints_, 0.0);
     effort_.assign(num_joints_, 0.0);
@@ -76,6 +78,9 @@ public:
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override
   {
     RCLCPP_INFO(rclcpp::get_logger("SNT_HW"), "SNT로봇 export_state_interfaces 작동 - 상태 인터페이스를 등록합니다.");
+
+    // 즉, JSB가  어떤  값을  읽어가면  될지  정의해준다. 
+
     std::vector<hardware_interface::StateInterface> state_interfaces;
     for (size_t i = 0; i < info_.joints.size(); ++i)
     {
@@ -92,6 +97,9 @@ public:
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override
   {
     RCLCPP_INFO(rclcpp::get_logger("SNT_HW"), "SNT로봇 export_command_interfaces 작동 - 명령 인터페이스를 등록합니다.");
+
+    // 즉, JTC가 어디다가  명령 값을  쓰면  되는지  정의해준다.
+
     std::vector<hardware_interface::CommandInterface> command_interfaces;
     for (size_t i = 0; i < info_.joints.size(); ++i)
     {
@@ -123,6 +131,9 @@ public:
 
     const double RAD_TO_DEG = 180.0 / 3.14159265358979323846;
     std::ostringstream ss;
+
+    // 어딘가에  쓴다고  가정하고 그냥  값 print
+    // rclcpp::get_logger 가  print역할을  하는 놈이다
 
     ss << "Joint Degrees: ";
     for (size_t i = 0; i < num_joints_; ++i)
